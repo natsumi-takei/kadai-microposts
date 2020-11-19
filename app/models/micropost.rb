fccd4d94802f7b3035cb6a4f379424afde8ed356
natsumi.takei <ruins_xxx@yahoo.co.jp>
2020-11-19 07:31:15 +0000

    favo destroy update

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id', dependent: :destroy
   
   validates :content, presence: true, length: { maximum: 255 }
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :likes, through: :favorites, source: :micropost
-  
+
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
